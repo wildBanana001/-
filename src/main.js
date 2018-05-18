6902e702bc2b12894e3b211943a953241b6461a7
lkf <563421209@qq.com>
2018-05-18 10:58:59 +0800

    小米全栈商城

---
+++ src/main.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueLazyload from 'vue-lazyload'
+import infiniteScroll from 'vue-infinite-scroll'
+import HelloWorld from '@/components/HelloWorld'
+import { currency } from './util/currency'
+
+import './assets/css/base.css'
+import './assets/css/checkout.css'
+import './assets/css/login.css'
+import './assets/css/product.css'
+
+Vue.use(infiniteScroll)
+Vue.use(Vuex)
+Vue.use(VueLazyload, {
+	loading: 'static/loading-svg/loading-bars/svg',
+	try: 3	//默认为1
+})
+
+Vue.filter('currency',currency)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
